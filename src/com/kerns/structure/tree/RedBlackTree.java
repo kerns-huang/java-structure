ae7fcc1534d1aa90ba0be9f4fd75fb20c9f6adca
黄春波 <yanlei3032@gmail.com>
2019-06-24 09:45:35 +0800

    添加自平衡的逻辑

--- src/com/kerns/structure/tree/RedBlackTree.java
+++ src/com/kerns/structure/tree/RedBlackTree.java
@@ -13,6 +13,7 @@ import com.oracle.webservices.internal.api.databinding.DatabindingMode;
  * (4) 如果一个节点是红色的，则它的子节点必须是黑色的。
  * (5) 从一个节点到该节点的子孙节点的所有路径上包含相同数目的黑节点。
  **/
+
 public class RedBlackTree<T extends Comparable<T>> {
 
     private RBTreeNode<T> root;
@@ -26,7 +27,27 @@ public class RedBlackTree<T extends Comparable<T>> {
       private RBTreeNode<T> right;
       private RBTreeNode<T> parent;
 
+        public RBTreeNode(boolean color, T key, RBTreeNode<T> left, RBTreeNode<T> right, RBTreeNode<T> parent) {
+            this.color = color;
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+        }
     }
+
+    /**
+     * 插入一条数据
+     * @param key
+     */
+    public void insert(T key) {
+        RBTreeNode<T> node=new RBTreeNode<T>(RED,key,null,null,null);
+        // 如果新建结点失败，则返回。
+        if (node != null)
+            insert(node);
+    }
+
+
     /*
      * 对红黑树的节点(x)进行左旋转，意味着x做为左节点存在
      *
@@ -144,5 +165,97 @@ public class RedBlackTree<T extends Comparable<T>> {
        }
        node.color=RED;
        //TODO 重新平衡，染色
+    }
+
+    private boolean isRed(RBTreeNode<T> node)
+    {
+        return RED==node.color;
+    }
+
+    public RBTreeNode parentOf(RBTreeNode<T> node)
+    {
+        return node.parent;
+    }
+
+    public void setBlack(RBTreeNode<T> node)
+    {
+        node.color=BLACK;
+    }
+
+    public void setRed(RBTreeNode<T> node)
+    {
+        node.color=RED;
+    }
+
+    /*
+     * 红黑树插入修正函数
+     *
+     * 在向红黑树中插入节点之后(失去平衡)，再调用该函数；
+     * 目的是将它重新塑造成一颗红黑树。
+     *
+     * 参数说明：
+     *     node 插入的结点        // 对应《算法导论》中的z
+     */
+    private void insertFixUp(RBTreeNode<T> node) {
+        RBTreeNode<T> parent, gparent;//父节点，祖父节点
+
+        // 若“父节点存在，并且父节点的颜色是红色”
+        while (((parent = node.parent)!=null) && isRed(parent)) {
+            gparent = parentOf(parent);
+
+            //若“父节点”是“祖父节点的左孩子”
+            if (parent == gparent.left) {
+                // Case 1条件：叔叔节点是红色
+                RBTreeNode<T> uncle = gparent.right;
+                if ((uncle!=null) && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(gparent);
+                    node = gparent;
+                    continue;
+                }
+
+                // Case 2条件：叔叔是黑色，且当前节点是右孩子
+                if (parent.right == node) {
+                    RBTreeNode<T> tmp;
+                    leftRotate(parent);
+                    tmp = parent;
+                    parent = node;
+                    node = tmp;
+                }
+
+                // Case 3条件：叔叔是黑色，且当前节点是左孩子。
+                setBlack(parent);
+                setRed(gparent);
+                rightRotate(gparent);
+            } else {    //若“z的父节点”是“z的祖父节点的右孩子”
+                // Case 1条件：叔叔节点是红色
+                RBTreeNode<T> uncle = gparent.left;
+                if ((uncle!=null) && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(gparent);
+                    node = gparent;
+                    continue;
+                }
+
+                // Case 2条件：叔叔是黑色，且当前节点是左孩子
+                if (parent.left == node) {
+                    RBTreeNode<T> tmp;
+                    rightRotate(parent);
+                    tmp = parent;
+                    parent = node;
+                    node = tmp;
+                }
+
+                // Case 3条件：叔叔是黑色，且当前节点是右孩子。
+                setBlack(parent);
+                setRed(gparent);
+                leftRotate(gparent);
+            }
+        }
+
+        // 将根节点设为黑色
+        setBlack(this.root);
     }
 }
