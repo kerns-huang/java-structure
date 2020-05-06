879944eda399ef9e3edca84d3b68dbc4a93d6c0a
xieshujie <xieshujie123>
2020-05-06 13:37:11 +0800

    m

--- src/test/java/com/kerns/structure/tree/BPlugsTreeTest.java
+++ src/test/java/com/kerns/structure/tree/BPlugsTreeTest.java
@@ -32,31 +32,26 @@ class BPlugsTreeTest {
     @Test
     public void testDeleteMerge(){
         BPlugsTree<Integer, String> bPlugsTree = new BPlugsTree(4);
-        bPlugsTree.insert(1, "test 1");
-        bPlugsTree.insert(2, "test 2");
-        bPlugsTree.insert(3, "test 3");
-        bPlugsTree.insert(4, "test 4");
-        bPlugsTree.insert(5, "test 5");
-        bPlugsTree.insert(6, "test 6");
-        bPlugsTree.insert(7, "test 7");
-        bPlugsTree.insert(8, "test 8");
-        bPlugsTree.insert(9, "test 9");
-        bPlugsTree.insert(10, "test 10");
-        bPlugsTree.insert(11, "test 11");
-        bPlugsTree.insert(12, "test 12");
-        bPlugsTree.insert(13, "test 13");
-        bPlugsTree.insert(14, "test 14");
-        bPlugsTree.insert(15, "test 15");
-        bPlugsTree.insert(16, "test 16");
-        bPlugsTree.insert(17, "test 17");
-        bPlugsTree.insert(18, "test 18");
-        bPlugsTree.insert(19, "test 19");
-        bPlugsTree.insert(20, "test 20");
+        for(int i=1;i<=20;i++){
+            bPlugsTree.insert(i, "test "+i);
+        }
         bPlugsTree.delete(3);
         Assertions.assertEquals(19,bPlugsTree.size());
         Assertions.assertEquals(null,bPlugsTree.get(3));
     }
 
+    @Test
+    public void testDelAll(){
+        BPlugsTree<Integer, String> bPlugsTree = new BPlugsTree(4);
+        for(int i=1;i<=20;i++){
+            bPlugsTree.insert(i, "test "+i);
+        }
+        for(int i=1;i<=20;i++){
+            bPlugsTree.delete(i);
+        }
+        Assertions.assertTrue(bPlugsTree.isEmpty());
+        Assertions.assertEquals(null,bPlugsTree.get(3));
+    }
 
     @Test
     public void testFistInsert() {
